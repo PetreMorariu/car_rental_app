ca5b44815ccaa5c0ab96a75f56eabc9faba5c525
pmorariu <morariupetru@yahoo.com>
2025-05-10 22:55:17 +0300

    added a Car model for the cars and populated the database with 30 plus cars from cars.json

--- car_rental/models.py
+++ car_rental/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    location = models.CharField(max_length=100)
+    price = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.make} {self.model}"
--- car_rental/views.py
+++ car_rental/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Car
 
 def home(request):
-    return render(request, 'car_rental/home.html')
+    cars = Car.objects.all()
+    return render(request, 'car_rental/home.html',{'cars':cars})
+
+
+
