8bcbfcaa840bafed422477380cc7a4d1b6c9aefd
pmorariu <morariupetru@yahoo.com>
2025-05-12 13:58:56 +0300

    added the add a new car functionality

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='car_rental-home')
+    path('',views.home,name='car_rental-home'),
+    path('add_car/',views.add_car,name='add_car'),
 ]
--- car_rental/views.py
+++ car_rental/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def home(request):
     cars = Car.objects.all()
     return render(request, 'car_rental/home.html',{'cars':cars})
 
-
-
+def add_car(request):
+    if request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your recipe has been created!')
+            return redirect('car_rental-home')
+        else:
+            messages.error(f'Correct the issues!')
+            return redirect(request,'car_rental/add_car.html',{'form':form})
+    else:
+        form = CarForm()
+        return redirect(request, 'car_rental/add_car.html', {'form': form})
