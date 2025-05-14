675215b5a0c00aea6f219b99d90b1169a4a00e54
pmorariu <morariupetru@yahoo.com>
2025-05-14 11:13:29 +0300

    added view Customers functionality

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='car_rental-home'),
+    path('customers/',views.customer_view,name='customers'),
     path('car/add/',views.add_car,name='add_car'),
     path('car/<int:car_id>/edit/',views.edit_car,name='edit_car'),
     path('car/<int:car_id>/delete/',views.delete_car,name='delete_car'),
--- car_rental/views.py
+++ car_rental/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Car
+from .models import Car,Customers
 from .forms import CarForm
 from django.contrib import messages
 
 def home(request):
     cars = Car.objects.all()
     return render(request, 'car_rental/home.html',{'cars':cars})
+
+def customer_view(request):
+    customers = Customers.objects.all()
+    return render(request,'car_rental/customers_list.html',{'customers':customers})
 
 def detail_view_car(request, car_id:int):
     car = get_object_or_404(Car, id=car_id)
