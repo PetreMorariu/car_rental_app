8a8f197f613ac84d46c7584565d60922925a5489
pmorariu <morariupetru@yahoo.com>
2025-05-14 11:28:08 +0300

    added functionality for add a new customer

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='car_rental-home'),
     path('customers/',views.customer_view,name='customers'),
+    path('customers/add/',views.customer_add_view,name='add_customers'),
     path('car/add/',views.add_car,name='add_car'),
     path('car/<int:car_id>/edit/',views.edit_car,name='edit_car'),
     path('car/<int:car_id>/delete/',views.delete_car,name='delete_car'),
--- car_rental/views.py
+++ car_rental/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Car,Customers
-from .forms import CarForm
+from .forms import CarForm,CustomerForm
 from django.contrib import messages
 
 def home(request):
     cars = Car.objects.all()
     return render(request, 'car_rental/home.html',{'cars':cars})
-
-def customer_view(request):
-    customers = Customers.objects.all()
-    return render(request,'car_rental/customers_list.html',{'customers':customers})
 
 def detail_view_car(request, car_id:int):
     car = get_object_or_404(Car, id=car_id)
@@ -51,6 +47,24 @@ def delete_car(request, car_id):
         return redirect("car_rental-home")
     return render(request, 'car_rental/confirm_delete.html', {'car': car})
 
+
+def customer_view(request):
+    customers = Customers.objects.all()
+    return render(request,'car_rental/customers_list.html',{'customers':customers})
+
+def customer_add_view(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,f'The customer was created!')
+            return redirect('customers')
+        else:
+            messages.error(request,f'Correct the issues!')
+            return render(request,'car_rental/customers_add.html',{'form':form})
+    else:
+        form = CustomerForm()
+        return render(request,'car_rental/customers_add.html',{'form':form})
 
 
 
