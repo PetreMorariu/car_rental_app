972843629c06223140d42943eae08a9c9d75f0dc
pmorariu <morariupetru@yahoo.com>
2025-05-15 10:23:36 +0300

    added Customers and bookins to the admin page and added a cancel button when adding any new objects

--- car_rental/admin.py
+++ car_rental/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car,Customers,Booking
 
 admin.site.register(Car)
+admin.site.register(Customers)
+admin.site.register(Booking)
